1b883173447d2fdbddb1d9afcc6d8a2f2f8aa078
In'Maldrerah Eyllisitryanmitore <i@httpssec.com>
2025-11-03 10:14:29 +0000

    Update cangjie build scripts to new nixpkgs (wip)

--- pkgs/cangjie/cangjie.nix
+++ pkgs/cangjie/cangjie.nix
@@ -1,5 +1,5 @@
 { cjver, cjsrcs, pkgs, lib, libedit, ncurses6, ... }:
-pkgs.llvmPackages_15.stdenv.mkDerivation {
+pkgs.llvmPackages.stdenv.mkDerivation {
   pname = "cangjie";
   version = cjver;
   srcs = cjsrcs;
@@ -8,17 +8,32 @@ pkgs.llvmPackages_15.stdenv.mkDerivation {
     libedit
     ncurses6
   ];
+  nativeBuildInputs = with pkgs; [
+    cmake
+    ninja
+    python3
+    git
+  ];
   postUnpack = ''
-    ln -s ../../flatbuffers cangjie-compiler/third_party/flatbuffers
-    ln -s ../../llvm-project cangjie-compiler/third_party/llvm-project
+    ln -s ../../flatbuffers cangjie_compiler/third_party/flatbuffers
+    ln -s ../../llvm-project cangjie_compiler/third_party/llvm-project
+    ln -s ../../libboundscheck cangjie_compiler/third_party/boundscheck-v1.1.16
+    ln -s ../../libxml2 cangjie_compiler/third_party/libxml2-v2.9.12
   '';
   postPatch = ''
-    git -C flatbuffers apply ../cangjie-compiler/third_party/flatbufferPatch.diff
-    git -C llvm-project apply ../cangjie-compiler/third_party/llvmPatch.diff
+    git -C flatbuffers apply ../cangjie_compiler/third_party/flatbufferPatch.diff
+    git -C llvm-project apply ../cangjie_compiler/third_party/llvmPatch.diff
+    # Find all .cpp and .hpp/.h files and add <cstdint> before <string>
+    find . -type f \( -name "*.cpp" -o -name "*.hpp" -o -name "*.h" \) | while read f; do
+      # Only modify files that actually include <string>
+      if grep -q -i -E '(u?int|float)(_fast|_least|max|ptr)?[0-9]*_(t|min|max)' "$f"; then
+        # Insert #include <cstdint> before the #include <string> line
+        sed -i 's|^|#include <cstdint>\n|' "$f"
+      fi
+  done
   '';
+  dontConfigure = true;
   buildPhase = ''
-    runhook preBuild;
-
     cd cangjie_compiler;
     python3 build.py clean;
     export CMAKE_PREFIX_PATH=${libedit}:${ncurses6};
@@ -27,7 +42,9 @@ pkgs.llvmPackages_15.stdenv.mkDerivation {
         --build-cjdb;
     python3 build.py install;
     cd -;
-
-    runhook postBuild
   '';
+  ARCH = "x86_64";
+  SDK_NAME = "linux-x64";
+  CANGJIE_VERSION = "1.0.0";
+  STDX_VERSION = "1";
 }
--- pkgs/cangjie/default.nix
+++ pkgs/cangjie/default.nix
@@ -10,43 +10,63 @@ let
   makeCangjiePkgs = argList: lib.mergeAttrsList (map makeCangjiePkg argList);
   cangjiePkgs = makeCangjiePkgs [
     {
-      cjver = "202507212144";
+      cjver = "1.0.0";
       cjsrcs = [
         (builtins.fetchGit {
-          name = "cangjie-compiler";
+          name = "cangjie_compiler";
           url = "ssh://git@gitcode.com/Cangjie/cangjie-compiler.git";
           ref = "release-cangjie-202507212144";
           rev = "623d6980f63fb8973a9de897c75be735f8f90918";
+          shallow = true;
         })
         (builtins.fetchGit {
-          name = "cangjie-runtime";
+          name = "cangjie_runtime";
           url = "ssh://git@gitcode.com/Cangjie/cangjie-runtime.git";
           ref = "release-cangjie-202507212144";
           rev = "f48eec88592c09cd98d33d338b4de4ee0ba38b1b";
+          shallow = true;
         })
         (builtins.fetchGit {
-          name = "cangjie-tools";
+          name = "cangjie_tools";
           url = "ssh://git@gitcode.com/Cangjie/cangjie-tools.git";
           ref = "release-cangjie-202507212144";
           rev = "32f7d75c27a0837819cf22d6793ba7fec8804348";
+          shallow = true;
         })
         (builtins.fetchGit {
           name = "stdx";
           url = "ssh://git@gitcode.com/Cangjie/stdx.git";
           ref = "release-cangjie-202507212144";
           rev = "3d6614a335de80d79fe711e7ede4a4ace2d6a655";
+          shallow = true;
         })
         (pkgs.fetchgit {
           name = "flatbuffers";
           url = "https://gitee.com/mirrors_trending/flatbuffers.git";
           rev = "v24.3.25";
           hash = "sha256-uE9CQnhzVgOweYLhWPn2hvzXHyBbFiFVESJ1AEM3BmA=";
+          leaveDotGit = true;
         })
-        (builtins.fetchGit {
+        (pkgs.fetchgit {
           name = "llvm-project";
           url = "https://gitee.com/openharmony/third_party_llvm-project.git";
           rev = "5c68a1cb123161b54b72ce90e7975d95a8eaf2a4";
-          shallow = true;
+          hash = "sha256-Gbf+WrWS62LBUGdvFQXDpMFSFRlEihOwxX63tp0+XF8=";
+          leaveDotGit = true;
+        })
+        (pkgs.fetchgit {
+          name = "libboundscheck";
+          url = "https://gitee.com/openeuler/libboundscheck.git";
+          rev = "v1.1.16";
+          hash = "sha256-cjztZQ1MbsMqWIi7Q2xAjzHX9/pIORy67ZX8dOUDa2g=";
+          leaveDotGit = true;
+        })
+        (pkgs.fetchgit {
+          name = "libxml2";
+          url = "https://gitee.com/mirrors/libxml2.git";
+          rev = "v2.9.12";
+          hash = "sha256-U5YGFgCpe0tXseQmAfL9VvBv6KPyujaGyWycqHEwLEw=";
+          leaveDotGit = true;
         })
       ];
     }
